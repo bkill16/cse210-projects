db13ecd7d224f23f651faedefcf550e200c7bc60
Brenleigh Killpack <kil21008@byui.edu>
2023-09-14 13:01:58 -0600

    Prep 2 Assignment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string response = Console.ReadLine();
+        int percent = int.Parse(response);
+
+        string letter = "";
+
+        if (percent >= 90)
+        {
+            letter = "A";
+        }
+        if (percent >= 80)
+        {
+            letter = "B";
+        }
+        if (percent >= 70)
+        {
+            letter = "C";
+        }
+        if (percent >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade: {letter}");
+
+        if (percent >= 70)
+        {
+            Console.Write("Congratulations! You passed this course.");
+        }
+        else
+        {
+            Console.Write("You did not pass this course. Do better next time.");
+        }
     }
 }
