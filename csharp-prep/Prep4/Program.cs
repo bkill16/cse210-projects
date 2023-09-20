3bedb4668db860c676e482f80bd6f2329eb3be46
Brenleigh Killpack <kil21008@byui.edu>
2023-09-19 18:14:12 -0600

    Prep 4 Assignment

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        int number = -1;
+        List<int> numbers = new List<int>();
+
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbers.Add(number);
+        }
+
+    int total = numbers.Sum();
+    Console.WriteLine($"The sum is: {total}");
+    double avg = Queryable.Average(numbers.AsQueryable());
+    Console.WriteLine($"The average is: {avg}");
+    int largest = numbers.Max();
+    Console.WriteLine($"The largest number is: {largest}");
     }
 }
