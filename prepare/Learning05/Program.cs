1be2b9b57adce6b87b6cbdce2d75aed9e7933674
Brenleigh Killpack <kil21008@byui.edu>
2023-11-06 15:26:41 -0700

    prep 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+        
+        Square sq1 = new Square("Blue", 5);
+        shapes.Add(sq1);
+
+        Rectangle rec1 = new Rectangle("Red", 4, 7);
+        shapes.Add(rec1);
+
+        Circle circle1 = new Circle("Yellow", 8);
+        shapes.Add(circle1);
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}");
+        }
     }
 }
